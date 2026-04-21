5edda0eef34c3f25e78f1e1f973710a9cc26814d
agerashchenko-pixel <a.gerashchenko@g.nsu.ru>
2026-04-21 19:25:34 +0700

    Update 6.py

--- 6.py
+++ 6.py
@@ -1,4 +1,9 @@
 def print_message(message):
+    """
+    Print the message or its first 160 characters if the length exceeds the limit.
+    :param message: the original string to be checked
+    :return: None
+    """
     if len(message) > 160:
         print(message[:160])
     else:
