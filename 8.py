baf08710d5d908aaa3037a0088a1fda2080b014f
agerashchenko-pixel <a.gerashchenko@g.nsu.ru>
2026-04-21 19:24:00 +0700

    Update 8.py

--- 8.py
+++ 8.py
@@ -2,6 +2,11 @@ from datetime import datetime
 
 
 def format_date(date):
+    """
+    Convert date string from one format to another and print it.
+    :param date: date string in format "%m/%d/%Y %H:%M:%S"
+    :return: None
+    """
     try:
         dt = datetime.strptime(date, "%m/%d/%Y %H:%M:%S")
         print(dt.strftime("%d.%m.%y %I:%M:%S %p"))
