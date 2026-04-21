471ec7ed44df782f29f04f2dac54fab962a1bb19
agerashchenko-pixel <a.gerashchenko@g.nsu.ru>
2026-04-21 19:12:31 +0700

    Update 10.py

--- 10.py
+++ 10.py
@@ -1,20 +1,22 @@
+def print_filtered_numbers(a, b):
+    if a > b:
+        a, b = b, a
+
+    symbols = "13489"
+
+    for num in range(a, b + 1):
+        num_str = str(num)
+        is_good = True
+
+        for digit in num_str:
+            if digit not in symbols:
+                is_good = False
+                break
+
+        if is_good:
+            print(num)
+
+
 a = int(input("Введите число A: "))
 b = int(input("Введите число B: "))
-
-if a > b:
-    a, b = b, a
-
-symbols = "13489"
-
-for num in range(a, b + 1):
-    num_str = str(num)
-
-    is_good = True
-
-    for digit in num_str:
-        if digit not in symbols:
-            is_good = False
-            break
-
-    if is_good:
-        print(num)
+print_filtered_numbers(a, b)
