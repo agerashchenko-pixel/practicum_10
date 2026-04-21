71f7afd959ece576231aa9fa81a3d77ec519d4b5
agerashchenko-pixel <a.gerashchenko@g.nsu.ru>
2026-04-21 19:15:39 +0700

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,9 +1,13 @@
-N = int(input())
-last_num = 1
-bl_num = 1
+def print_fibonacci(N):
+    last_num = 1
+    bl_num = 1
 
-for i in range(N):
-    print(bl_num)
-    incr_bl_num = bl_num
-    bl_num = last_num
-    last_num = incr_bl_num + last_num
+    for i in range(N):
+        print(bl_num)
+        incr_bl_num = bl_num
+        bl_num = last_num
+        last_num = incr_bl_num + last_num
+
+
+N = int(input())
+print_fibonacci(N)
