5b71be7193d8a3e59bdd5f95b0fc9683a28b83fe
agerashchenko-pixel <a.gerashchenko@g.nsu.ru>
2026-04-21 19:19:31 +0700

    Update 8.py

--- 8.py
+++ 8.py
@@ -1,9 +1,13 @@
 from datetime import datetime
 
-date = input()
 
-try:
-    dt = datetime.strptime(date, "%m/%d/%Y %H:%M:%S")
-    print(dt.strftime("%d.%m.%y %I:%M:%S %p"))
-except:
-    print("Неверная дата")
+def format_date(date):
+    try:
+        dt = datetime.strptime(date, "%m/%d/%Y %H:%M:%S")
+        print(dt.strftime("%d.%m.%y %I:%M:%S %p"))
+    except ValueError:
+        print("Неверная дата")
+
+
+date = input()
+format_date(date)
