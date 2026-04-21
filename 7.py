cd49fe8c4ec0f95682b413c81bb2b514f5f1a707
agerashchenko-pixel <a.gerashchenko@g.nsu.ru>
2026-04-21 19:24:29 +0700

    Update 7.py

--- 7.py
+++ 7.py
@@ -1,4 +1,11 @@
-def common_multiples(A, B, N):
+def print_common_multiples(A, B, N):
+    """
+    Print all common multiples of A and B up to N.
+    :param A: first divisor
+    :param B: second divisor
+    :param N: upper limit for checking
+    :return: None
+    """
     for i in range(1, N + 1):
         if i % A == 0 and i % B == 0:
             print(i)
@@ -7,4 +14,4 @@ def common_multiples(A, B, N):
 A = int(input())
 B = int(input())
 N = int(input())
-common_multiples(A, B, N)
+print_common_multiples(A, B, N)
