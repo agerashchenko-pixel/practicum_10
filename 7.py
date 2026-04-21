cb28d46a97784ce1d352d036f32cccb8e11788e4
agerashchenko-pixel <a.gerashchenko@g.nsu.ru>
2026-04-21 19:19:02 +0700

    Update 7.py

--- 7.py
+++ 7.py
@@ -1,7 +1,10 @@
+def common_multiples(A, B, N):
+    for i in range(1, N + 1):
+        if i % A == 0 and i % B == 0:
+            print(i)
+
+
 A = int(input())
 B = int(input())
 N = int(input())
-
-for i in range(1, N + 1):
-    if i % A == 0 and i % B == 0:
-        print(i)
+common_multiples(A, B, N)
