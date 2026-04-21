0c0c010d353cb9e46b4478a455b5d96838306a78
agerashchenko-pixel <a.gerashchenko@g.nsu.ru>
2026-04-21 19:14:53 +0700

    Update 11.py

--- 11.py
+++ 11.py
@@ -1,15 +1,18 @@
+def print_simple_numbers(N):
+    for num in range(1, N + 1):
+        if num < 2:
+            continue
+
+        simple = True
+
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                simple = False
+                break
+
+        if simple:
+            print(num)
+
+
 N = int(input())
-
-for num in range(1, N + 1):
-    if num < 2:
-        continue
-
-    simple = True
-
-    for i in range(2, int(num ** 0.5) + 1):
-        if num % i == 0:
-            simple = False
-            break
-
-    if simple:
-        print(num)
+print_simple_numbers(N)
